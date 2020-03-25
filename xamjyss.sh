9378d2b6036dcca0ee5e312de940abeeaf5d7773
xamjyss143 <62631365+xamjyss143@users.noreply.github.com>
2020-03-25 18:03:21 +0800

    Create xamjyss.sh

---
+++ xamjyss.sh
@@ -0,0 +1,15 @@
+  
+#!/bin/bash
+echo "Removing Old Theme"
+rm -rf /home/panel/html/assets
+rm -rf /home/panel/html/view
+rm -rf /home/panel/html/tmp/*
+echo "Installing XAMJYSSVPN Template"
+cd ~
+mkdir xamjyss
+cd xamjyss
+rm -rf *
+wget https://github.com/xamjyss143/ocspaneltemplate/raw/master/xamjyss.tar.gz && tar zxvf xamjyss.tar.gz
+mv assets /home/panel/html
+mv view /home/panel/html
+<h1 style="color: purple;"> XAMJYSSVPN Template Successfully Installed </h1>
