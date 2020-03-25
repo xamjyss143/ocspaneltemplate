60821b9d918a2add87a3a634735ecdb9c4996c42
xamjyss143 <62631365+xamjyss143@users.noreply.github.com>
2020-03-25 23:12:09 +0800

    Update and rename xamjyss02.sh to xamjyss03.sh

--- xamjyss03.sh
+++ xamjyss03.sh
@@ -11,6 +11,12 @@ mkdir xamjyss
 cd xamjyss
 rm -rf *
 wget https://github.com/xamjyss143/ocspaneltemplate/raw/master/xamjyss.tar.gz && tar zxvf xamjyss.tar.gz
+wget https://github.com/xamjyss143/ocspaneltemplate/raw/master/memberconfigs.tar.gz && tar zxvf memberconfigs.tar.gz
+mv configs.php /home/panel/html/controller/member
+wget https://github.com/xamjyss143/ocspaneltemplate/raw/master/helperconfigs.tar.gz && tar zxvf helperconfigs.tar.gz
+mv configs.php /home/panel/html/controller/helper
+wget https://github.com/xamjyss143/ocspaneltemplate/raw/master/modelconfigs.tar.gz && tar zxvf modelconfigs.tar.gz
+mv configs.php /home/panel/html/model
 wget https://github.com/xamjyss143/ocspaneltemplate/raw/master/controllerprofile.tar.gz && tar zxvf controllerprofile.tar.gz
 mv profile.php /home/panel/html/controller/member
 wget https://github.com/xamjyss143/ocspaneltemplate/raw/master/controllerhelperprofile.tar.gz && tar zxvf controllerhelperprofile.tar.gz
